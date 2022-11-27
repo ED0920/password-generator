7bd406a3e21e6d570f0111def028a9bb924c7770
Elisa <elisa.do@outlook.com>
2022-11-27 23:47:31 +1100

    created alpha/numeric characters used to generate password

--- Assets/script.js
+++ Assets/script.js
@@ -21,36 +21,50 @@ function generatePassword() {
   var numChars = window.prompt("How many charaters would you like your password to contain?");
       
 
-if(numChars < minPasswordLength){
-  console.log("min length 8 characters");
-  window.alert("min length 8 characters");
-  return null
-}
+  if(numChars < minPasswordLength){
+    console.log("min length 8 characters");
+    window.alert("min length 8 characters");
+    return null
+  }
 
-if(numChars > maxPasswordLength){
-  console.log("max length 128 characters");
-  window.alert("max length 128 characters");
-  return null
-}
+  if(numChars > maxPasswordLength){
+    console.log("max length 128 characters");
+    window.alert("max length 128 characters");
+    return null
+  }
 
-var requireLowercase = window.prompt("Do you require a lowercase? (Y/N)");
-if (requireLowercase !== "Y" || requireLowercase !== "N" ){
-  return null
-}
+  var requireLowercase = window.prompt("Do you require a lowercase? (Y/N)");
+  if (requireLowercase !== "Y" || requireLowercase !== "N" ){
+    return null
+  }
 
-var requireUppercase = window.prompt("Do you require an uppercase?");
-if (requireUppercase !== "Y" || requireUppercase !== "N" ){
-  return null
-}
-var requireNumeric = window.prompt("Do you require a numbers? (Y/N)");
-if (requireNumeric !== "Y" || requireNumeric !== "N" ){
-  return null
-}
-var requireSpecialChar = window.prompt("Do you require special characters?");
-if (requireSpecialChar !== "Y" || requireSpecialChar !== "N" ){
-  return null
-}
+  var requireUppercase = window.prompt("Do you require an uppercase?");
+  if (requireUppercase !== "Y" || requireUppercase !== "N" ){
+    return null
+  }
+  var requireNumeric = window.prompt("Do you require a numbers? (Y/N)");
+  if (requireNumeric !== "Y" || requireNumeric !== "N" ){
+    return null
+  }
+  var requireSpecialChar = window.prompt("Do you require special characters?");
+  if (requireSpecialChar !== "Y" || requireSpecialChar !== "N" ){
+    return null
+  }
 
+  // "acceptedChars +=" acceptedChars + letters   
+ var acceptedChars = ""
+  if (requireLowercase == "Y"){
+    acceptedChars += letters; 
+  }
+  if (requireUppercase == "Y"){
+    acceptedChars += letters.toUpperCase();
+  }
+  if (requireNumeric == "Y"){
+    acceptedChars += numeric
+  }
+  if (requireSpecialChar == "Y"){
+    acceptedChars += specialChars
+  }
 
 }
 
